37185bd151dd39bb0e44f41ed16e28266e5114ca
Sherry El Garhy <sherryptk@gmail.com>
2017-11-18 22:48:00 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,13 @@
 class Author
   attr_accessor :name, :posts
+
   def initialize(name)
     @name = name
     @posts = []
+  end
+
+  def add_post(post)
+    @posts<<post
+    post.name = self
   end
 end
