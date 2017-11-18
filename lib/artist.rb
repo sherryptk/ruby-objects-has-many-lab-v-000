fe04375b2d1213377e8092112f22d6dad9e1aac6
Sherry El Garhy <sherryptk@gmail.com>
2017-11-18 22:09:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,11 @@
 class Artist
    attr_accessor :name
 
-   @@all = []
+   @@song_count = 0
 
   def initialize(name)
     @name = name
     @songs = []
-    @@all << name
   end
 
   def songs
@@ -15,12 +14,14 @@ class Artist
 
   def add_song(song)
     @songs << song
+    @@song_count+=1
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
+    @@song_count+=1
     song.artist = self
   end
 
